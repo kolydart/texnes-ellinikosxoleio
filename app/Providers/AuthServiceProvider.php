8d60be4e483ad0f08bb6b8c8bb448eb1eb9d972d
kolydart <kolydart@macbeth.gateweb.gr>
2018-07-31 20:08:18 +0300

    fix Papers acl

--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -30,6 +30,23 @@ class AuthServiceProvider extends ServiceProvider
         $user = \Auth::user();
 
         
+        // Auth gates for: Papers
+        Gate::define('paper_access', function ($user) {
+            return in_array($user->role_id, [1, 3, 4, 5]);
+        });
+        Gate::define('paper_create', function ($user) {
+            return in_array($user->role_id, [1, 3]);
+        });
+        Gate::define('paper_edit', function ($user) {
+            return in_array($user->role_id, [1, 3]);
+        });
+        Gate::define('paper_view', function ($user) {
+            return in_array($user->role_id, [1, 3, 4, 5]);
+        });
+        Gate::define('paper_delete', function ($user) {
+            return in_array($user->role_id, [1, 3]);
+        });
+
         // Auth gates for: Reviews
         Gate::define('review_access', function ($user) {
             return in_array($user->role_id, [1, 3, 4, 5]);
--- config/app.php
+++ config/app.php
@@ -185,6 +185,7 @@ return [
         Laravel\Tinker\TinkerServiceProvider::class,
         Intervention\Image\ImageServiceProvider::class,
         Collective\Html\HtmlServiceProvider::class,
+        Unisharp\Laravelfilemanager\LaravelFilemanagerServiceProvider::class,
         Spatie\MediaLibrary\MediaLibraryServiceProvider::class,
         
         /*
