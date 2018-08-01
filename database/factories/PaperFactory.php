2f4b9370945bdb27473c93d3d32ae4674a8db326
kolydart <kolydart@macbeth.gateweb.gr>
2018-08-01 14:06:35 +0300

    re-apply 1.7

--- database/factories/PaperFactory.php
+++ database/factories/PaperFactory.php
@@ -1,15 +1,18 @@
 <?php
 
 $factory->define(App\Paper::class, function (Faker\Generator $faker) {
+    $types = ["Εισήγηση","Εργαστήριο: βιωματικές δράσεις","Εργαστήριο: καλές πρακτικές",];
+    $durations = ["20","45","90",];
+    $i = collect([0,1,2])->random();
     return [
-        "title" => $faker->name,
-        "type" => collect(["Εισήγηση","Εργαστήριο: βιωματικές δράσεις","Εργαστήριο: καλές πρακτικές",])->random(),
-        "duration" => collect(["20","45","90",])->random(),
+        "title" => $faker->sentence,
+        "type" => $types[$i],
+        "duration" => $durations[$i],
         "name" => $faker->name,
         "email" => $faker->safeEmail,
         "attribute" => collect(["Πανεπιστημιακός","Ερευνητής","Μεταπτυχιακός φοιτητής","Εκπαιδευτικός Β/θμιας Εκπ/σης","Εκπαιδευτικός Α/θμιας Εκπ/σης","Καλλιτέχνης","Άλλο",])->random(),
         "phone" => $faker->name,
         "status" => collect(["Accepted","Rejected","Pending",])->random(),
-        "informed" => collect(["Unaware","Informed",])->random(),
+        "informed" => collect(['Unaware','Informed'])->random(),
     ];
 });
--- database/factories/ReviewFactory.php
+++ database/factories/ReviewFactory.php
@@ -5,6 +5,7 @@ $factory->define(App\Review::class, function (Faker\Generator $faker) {
         "user_id" => factory('App\User')->create(),
         "paper_id" => factory('App\Paper')->create(),
         "review" => collect(["Approve","Neutral","Reject",])->random(),
-        "comment" => $faker->name,
+        "comment" => $faker->paragraph,
+        "user_id" => collect([1,2,3,4])->random(),
     ];
 });
