9cf67f8880107c2128b2fd55c22b6863973c23db
kolydart <kolydart@macbeth.gateweb.gr>
2018-08-04 14:30:04 +0300

    create new calendar for sessions

--- app/Http/Controllers/Admin/SystemCalendarController.php
+++ app/Http/Controllers/Admin/SystemCalendarController.php
@@ -29,6 +29,24 @@ class SystemCalendarController extends Controller
            ]; 
         } 
 
+        foreach (\App\Session::all() as $session) { 
+           $crudFieldValue = $session->getOriginal('start'); 
+
+           if (! $crudFieldValue) {
+               continue;
+           }
+
+           $eventLabel     = $session->title; 
+           $prefix         = ''; 
+           $suffix         = ''; 
+           $dataFieldValue = trim($prefix . " " . $eventLabel . " " . $suffix); 
+           $events[]       = [ 
+                'title' => $dataFieldValue, 
+                'start' => $crudFieldValue, 
+                'url'   => route('admin.sessions.edit', $session->id)
+           ]; 
+        } 
+
 
        return view('admin.calendar' , compact('events')); 
     }
