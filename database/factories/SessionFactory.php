2faf313a62a9ca68d4fc29ced4fa4ebea04aeb93
kolydart <kolydart@macbeth.gateweb.gr>
2018-08-05 08:18:54 +0300

    session factory improve

--- database/factories/SessionFactory.php
+++ database/factories/SessionFactory.php
@@ -1,15 +1,16 @@
 <?php
 
 use App\Room;
+use gateweb\common\DateTime;
 
 $factory->define(App\Session::class, function (Faker\Generator $faker) {
-	$date = $faker->dateTimeBetween($startDate = '2018-10-11', $endDate = '2018-10-14');
-	$time = $faker->numberBetween(10,18);
+	$date = $faker->dateTimeBetween($startDate = '2018-10-11', $endDate = '2018-10-14')->format('Y-m-d H:m:s');
+	$obj = (new DateTime($date))->setTime($faker->numberBetween(10,18),00);
+
     return [
         "title" => 'Session '.$faker->unique()->numberBetween(100,300).$faker->randomLetter(),
         "room_id" => Room::all()->random()->id,
-        "start" => $date->format('Y-m-d ').$time.':00:00',
-        "end" =>  $date->format('Y-m-d ').($time+2).':00:00',
-        // "end" =>  $date->modify('+2 hours')->format('Y-m-d H:00:00'),
+        "start" => $obj->sql(),
+        "end" =>  $obj->modify('+2 hours')->sql(),
     ];
 });
