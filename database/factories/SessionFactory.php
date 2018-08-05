2bf27f69ec1e3e959531145de8c1c22c0fbd37ba
kolydart <kolydart@macbeth.gateweb.gr>
2018-08-05 07:57:51 +0300

    session factory, faker produce random datetime between 10-18

--- database/factories/SessionFactory.php
+++ database/factories/SessionFactory.php
@@ -3,11 +3,13 @@
 use App\Room;
 
 $factory->define(App\Session::class, function (Faker\Generator $faker) {
-	$start = $faker->dateTimeBetween($startDate = '2018-10-11', $endDate = '2018-10-14');
+	$date = $faker->dateTimeBetween($startDate = '2018-10-11', $endDate = '2018-10-14');
+	$time = $faker->numberBetween(10,18);
     return [
         "title" => 'Session '.$faker->unique()->numberBetween(100,300).$faker->randomLetter(),
         "room_id" => Room::all()->random()->id,
-        "start" => $start->format('Y-m-d H:00:00'),
-        "end" =>  $start->modify('+2 hours')->format('Y-m-d H:00:00'),
+        "start" => $date->format('Y-m-d ').$time.':00:00',
+        "end" =>  $date->format('Y-m-d ').($time+2).':00:00',
+        // "end" =>  $date->modify('+2 hours')->format('Y-m-d H:00:00'),
     ];
 });
