e6e5e677eb9090ae8b68f8674fc6b03f6babf044
kolydart <kolydart@macbeth.gateweb.gr>
2018-09-14 15:33:23 +0300

    frontend routes

--- routes/gw.php
+++ routes/gw.php
@@ -1,3 +1,22 @@
 <?php
 
 Route::get('/', 'Frontend\HomeController@index')->name('frontend.home');
+
+Route::get('/contact', 'Frontend\HomeController@contact')->name('frontend.contact');
+
+Route::get('arts', 'Frontend\ArtsController@index')->name('frontend.arts.index');
+Route::get('arts/{id}', 'Frontend\ArtsController@show')->name('frontend.arts.show');
+
+Route::get('papers', 'Frontend\PapersController@index')->name('frontend.papers.index');
+Route::get('papers/{id}', 'Frontend\PapersController@show')->name('frontend.papers.show');
+
+Route::get('rooms', 'Frontend\RoomsController@index')->name('frontend.rooms.index');
+Route::get('rooms/{id}', 'Frontend\RoomsController@show')->name('frontend.rooms.show');
+
+Route::get('sessions', 'Frontend\SessionsController@index')->name('frontend.sessions.index');
+Route::get('sessions/{id}', 'Frontend\SessionsController@show')->name('frontend.sessions.show');
+
+Route::get('/calendar', 'Frontend\SystemCalendarController@index')->name('frontend.calendar'); 
+
+Route::get('/page/{alias}', 'Frontend\ContentPagesController@show')->name('frontend.pages.show');
+
