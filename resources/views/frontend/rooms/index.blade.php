af9d277fe0a5155fc22431219b14d91f071aa60c
kolydart <kolydart@macbeth.gateweb.gr>
2018-09-15 18:24:21 +0300

    room view, ok

--- resources/views/frontend/rooms/index.blade.php
+++ resources/views/frontend/rooms/index.blade.php
@@ -3,22 +3,6 @@
 
 @section('content')
     <h3 class="page-title">@lang('quickadmin.rooms.title')</h3>
-    @can('room_create')
-    <p>
-        <a href="{{ route('frontend.rooms.create') }}" class="btn btn-success">@lang('quickadmin.qa_add_new')</a>
-        
-    </p>
-    @endcan
-
-    @can('room_delete')
-    <p>
-        <ul class="list-inline">
-            <li><a href="{{ route('frontend.rooms.index') }}" style="{{ request('show_deleted') == 1 ? '' : 'font-weight: 700' }}">@lang('quickadmin.qa_all')</a></li> |
-            <li><a href="{{ route('frontend.rooms.index') }}?show_deleted=1" style="{{ request('show_deleted') == 1 ? 'font-weight: 700' : '' }}">@lang('quickadmin.qa_trash')</a></li>
-        </ul>
-    </p>
-    @endcan
-
 
     <div class="panel panel-default">
         <div class="panel-heading">
@@ -26,22 +10,12 @@
         </div>
 
         <div class="panel-body table-responsive">
-            <table class="table table-bordered table-striped {{ count($rooms) > 0 ? 'datatable' : '' }} @can('room_delete') @if ( request('show_deleted') != 1 ) dt-select @endif @endcan">
+            <table class="table table-bordered table-striped {{ count($rooms) > 0 ? 'datatable' : '' }}">
                 <thead>
                     <tr>
-                        @can('room_delete')
-                            @if ( request('show_deleted') != 1 )<th style="text-align:center;"><input type="checkbox" id="select-all" /></th>@endif
-                        @endcan
-
                         <th>@lang('quickadmin.rooms.fields.title')</th>
-                        <th>@lang('quickadmin.rooms.fields.description')</th>
                         <th>@lang('quickadmin.rooms.fields.type')</th>
-                        <th>@lang('quickadmin.rooms.fields.wifi')</th>
-                        @if( request('show_deleted') == 1 )
-                        <th>&nbsp;</th>
-                        @else
-                        <th>&nbsp;</th>
-                        @endif
+                        <th></th>
                     </tr>
                 </thead>
                 
@@ -49,54 +23,10 @@
                     @if (count($rooms) > 0)
                         @foreach ($rooms as $room)
                             <tr data-entry-id="{{ $room->id }}">
-                                @can('room_delete')
-                                    @if ( request('show_deleted') != 1 )<td></td>@endif
-                                @endcan
 
                                 <td field-key='title'>{{ $room->title }}</td>
-                                <td field-key='description'>{!! $room->description !!}</td>
                                 <td field-key='type'>{{ $room->type }}</td>
-                                <td field-key='wifi'>{{ $room->wifi }}</td>
-                                @if( request('show_deleted') == 1 )
-                                <td>
-                                    @can('room_delete')
-                                                                        {!! Form::open(array(
-                                        'style' => 'display: inline-block;',
-                                        'method' => 'POST',
-                                        'onsubmit' => "return confirm('".trans("quickadmin.qa_are_you_sure")."');",
-                                        'route' => ['admin.rooms.restore', $room->id])) !!}
-                                    {!! Form::submit(trans('quickadmin.qa_restore'), array('class' => 'btn btn-xs btn-success')) !!}
-                                    {!! Form::close() !!}
-                                @endcan
-                                    @can('room_delete')
-                                                                        {!! Form::open(array(
-                                        'style' => 'display: inline-block;',
-                                        'method' => 'DELETE',
-                                        'onsubmit' => "return confirm('".trans("quickadmin.qa_are_you_sure")."');",
-                                        'route' => ['admin.rooms.perma_del', $room->id])) !!}
-                                    {!! Form::submit(trans('quickadmin.qa_permadel'), array('class' => 'btn btn-xs btn-danger')) !!}
-                                    {!! Form::close() !!}
-                                @endcan
-                                </td>
-                                @else
-                                <td>
-                                    @can('room_view')
-                                    <a href="{{ route('frontend.rooms.show',[$room->id]) }}" class="btn btn-xs btn-primary">@lang('quickadmin.qa_view')</a>
-                                    @endcan
-                                    @can('room_edit')
-                                    <a href="{{ route('frontend.rooms.edit',[$room->id]) }}" class="btn btn-xs btn-info">@lang('quickadmin.qa_edit')</a>
-                                    @endcan
-                                    @can('room_delete')
-{!! Form::open(array(
-                                        'style' => 'display: inline-block;',
-                                        'method' => 'DELETE',
-                                        'onsubmit' => "return confirm('".trans("quickadmin.qa_are_you_sure")."');",
-                                        'route' => ['admin.rooms.destroy', $room->id])) !!}
-                                    {!! Form::submit(trans('quickadmin.qa_delete'), array('class' => 'btn btn-xs btn-danger')) !!}
-                                    {!! Form::close() !!}
-                                    @endcan
-                                </td>
-                                @endif
+                                <td> <a href="{{ route('frontend.rooms.show',[$room->id]) }}" class="btn btn-xs btn-primary">@lang('quickadmin.qa_view')</a> </td>
                             </tr>
                         @endforeach
                     @else
@@ -109,12 +39,3 @@
         </div>
     </div>
 @stop
-
-@section('javascript') 
-    <script>
-        @can('room_delete')
-            @if ( request('show_deleted') != 1 ) window.route_mass_crud_entries_destroy = '{{ route('frontend.rooms.mass_destroy') }}'; @endif
-        @endcan
-
-    </script>
-@endsection
--- resources/views/frontend/rooms/show.blade.php
+++ resources/views/frontend/rooms/show.blade.php
@@ -10,7 +10,7 @@
 
         <div class="panel-body table-responsive">
             <div class="row">
-                <div class="col-md-6">
+                <div class="col-md-12">
                     <table class="table table-bordered table-striped">
                         <tr>
                             <th>@lang('quickadmin.rooms.fields.title')</th>
@@ -24,37 +24,29 @@
                             <th>@lang('quickadmin.rooms.fields.type')</th>
                             <td field-key='type'>{{ $room->type }}</td>
                         </tr>
-                        <tr>
-                            <th>@lang('quickadmin.rooms.fields.wifi')</th>
-                            <td field-key='wifi'>{{ $room->wifi }}</td>
-                        </tr>
                     </table>
                 </div>
             </div>
-    {{-- Χάρτες - πρόσβαση: http://texnes-ellinikosxoleio.uoa.gr/xartes-prosbash.html --}}
-            <!-- Nav tabs -->
-<ul class="nav nav-tabs" role="tablist">
+
+<a href="{{route('frontend.pages.show','map')}}" class="btn btn-primary m-3"><i class="fa fa-map"></i> Χάρτες - πρόσβαση</a>
+
     
-<li role="presentation" class="active"><a href="#sessions" aria-controls="sessions" role="tab" data-toggle="tab">Συνεδρίες</a></li>
-<li role="presentation" class=""><a href="#availability" aria-controls="availability" role="tab" data-toggle="tab">Διαθεσιμότητα αιθουσών</a></li>
+        <!-- Nav tabs -->
+<ul class="nav nav-tabs" role="tablist">
+    <li role="presentation" class="active"><a href="#sessions" aria-controls="sessions" role="tab" data-toggle="tab">Συνεδρίες</a></li>
 </ul>
 
 <!-- Tab panes -->
 <div class="tab-content">
-    
 <div role="tabpanel" class="tab-pane active" id="sessions">
 <table class="table table-bordered table-striped {{ count($sessions) > 0 ? 'datatable' : '' }}">
     <thead>
         <tr>
             <th>@lang('quickadmin.sessions.fields.title')</th>
-                        <th>@lang('quickadmin.sessions.fields.room')</th>
-                        <th>@lang('quickadmin.sessions.fields.start')</th>
-                        <th>@lang('quickadmin.sessions.fields.duration')</th>
-                        @if( request('show_deleted') == 1 )
-                        <th>&nbsp;</th>
-                        @else
-                        <th>&nbsp;</th>
-                        @endif
+            <th>@lang('quickadmin.sessions.fields.room')</th>
+            <th>@lang('quickadmin.sessions.fields.start')</th>
+            <th>@lang('quickadmin.sessions.fields.duration')</th>
+            <th></th>
         </tr>
     </thead>
 
@@ -63,49 +55,10 @@
             @foreach ($sessions as $session)
                 <tr data-entry-id="{{ $session->id }}">
                     <td field-key='title'><a href="{{route('frontend.sessions.show',$session->id)}}">{{ "S". $session->id.". ".$session->title }}</a></td>
-                                <td field-key='room'>{{ $session->room->title or '' }}</td>
-                                <td field-key='start'>{{ $session->start }}</td>
-                                <td field-key='duration'>{{ $session->duration }}</td>
-                                @if( request('show_deleted') == 1 )
-                                <td>
-                                    @can('session_delete')
-                                                                        {!! Form::open(array(
-                                        'style' => 'display: inline-block;',
-                                        'method' => 'POST',
-                                        'onsubmit' => "return confirm('".trans("quickadmin.qa_are_you_sure")."');",
-                                        'route' => ['admin.sessions.restore', $session->id])) !!}
-                                    {!! Form::submit(trans('quickadmin.qa_restore'), array('class' => 'btn btn-xs btn-success')) !!}
-                                    {!! Form::close() !!}
-                                @endcan
-                                    @can('session_delete')
-                                                                        {!! Form::open(array(
-                                        'style' => 'display: inline-block;',
-                                        'method' => 'DELETE',
-                                        'onsubmit' => "return confirm('".trans("quickadmin.qa_are_you_sure")."');",
-                                        'route' => ['admin.sessions.perma_del', $session->id])) !!}
-                                    {!! Form::submit(trans('quickadmin.qa_permadel'), array('class' => 'btn btn-xs btn-danger')) !!}
-                                    {!! Form::close() !!}
-                                @endcan
-                                </td>
-                                @else
-                                <td>
-                                    @can('session_view')
-                                    <a href="{{ route('frontend.sessions.show',[$session->id]) }}" class="btn btn-xs btn-primary">@lang('quickadmin.qa_view')</a>
-                                    @endcan
-                                    @can('session_edit')
-                                    <a href="{{ route('frontend.sessions.edit',[$session->id]) }}" class="btn btn-xs btn-info">@lang('quickadmin.qa_edit')</a>
-                                    @endcan
-                                    @can('session_delete')
-{!! Form::open(array(
-                                        'style' => 'display: inline-block;',
-                                        'method' => 'DELETE',
-                                        'onsubmit' => "return confirm('".trans("quickadmin.qa_are_you_sure")."');",
-                                        'route' => ['admin.sessions.destroy', $session->id])) !!}
-                                    {!! Form::submit(trans('quickadmin.qa_delete'), array('class' => 'btn btn-xs btn-danger')) !!}
-                                    {!! Form::close() !!}
-                                    @endcan
-                                </td>
-                                @endif
+                    <td field-key='room'>{{ $session->room->title or '' }}</td>
+                    <td field-key='start'>{{ $session->start }}</td>
+                    <td field-key='duration'>{{ $session->duration }}</td>
+                    <td> <a href="{{ route('frontend.sessions.show',[$session->id]) }}" class="btn btn-xs btn-primary">@lang('quickadmin.qa_view')</a> </td>
                 </tr>
             @endforeach
         @else
@@ -116,103 +69,12 @@
     </tbody>
 </table>
 </div>
-<div role="tabpanel" class="tab-pane " id="availability">
-<table class="table table-bordered table-striped {{ count($availabilities) > 0 ? 'datatable' : '' }}">
-    <thead>
-        <tr>
-            <th>@lang('quickadmin.availability.fields.type')</th>
-                        <th>@lang('quickadmin.availability.fields.start')</th>
-                        <th>@lang('quickadmin.availability.fields.end')</th>
-                        <th>@lang('quickadmin.availability.fields.room')</th>
-                        <th>@lang('quickadmin.availability.fields.notes')</th>
-                        @if( request('show_deleted') == 1 )
-                        <th>&nbsp;</th>
-                        @else
-                        <th>&nbsp;</th>
-                        @endif
-        </tr>
-    </thead>
-
-    <tbody>
-        @if (count($availabilities) > 0)
-            @foreach ($availabilities as $availability)
-                <tr data-entry-id="{{ $availability->id }}">
-                    <td field-key='type'>{{ $availability->type }}</td>
-                                <td field-key='start'>{{ $availability->start }}</td>
-                                <td field-key='end'>{{ $availability->end }}</td>
-                                <td field-key='room'>{{ $availability->room->title or '' }}</td>
-                                <td field-key='notes'>{!! $availability->notes !!}</td>
-                                @if( request('show_deleted') == 1 )
-                                <td>
-                                    @can('availability_delete')
-                                                                        {!! Form::open(array(
-                                        'style' => 'display: inline-block;',
-                                        'method' => 'POST',
-                                        'onsubmit' => "return confirm('".trans("quickadmin.qa_are_you_sure")."');",
-                                        'route' => ['admin.availabilities.restore', $availability->id])) !!}
-                                    {!! Form::submit(trans('quickadmin.qa_restore'), array('class' => 'btn btn-xs btn-success')) !!}
-                                    {!! Form::close() !!}
-                                @endcan
-                                    @can('availability_delete')
-                                                                        {!! Form::open(array(
-                                        'style' => 'display: inline-block;',
-                                        'method' => 'DELETE',
-                                        'onsubmit' => "return confirm('".trans("quickadmin.qa_are_you_sure")."');",
-                                        'route' => ['admin.availabilities.perma_del', $availability->id])) !!}
-                                    {!! Form::submit(trans('quickadmin.qa_permadel'), array('class' => 'btn btn-xs btn-danger')) !!}
-                                    {!! Form::close() !!}
-                                @endcan
-                                </td>
-                                @else
-                                <td>
-                                    @can('availability_view')
-                                    <a href="{{ route('frontend.availabilities.show',[$availability->id]) }}" class="btn btn-xs btn-primary">@lang('quickadmin.qa_view')</a>
-                                    @endcan
-                                    @can('availability_edit')
-                                    <a href="{{ route('frontend.availabilities.edit',[$availability->id]) }}" class="btn btn-xs btn-info">@lang('quickadmin.qa_edit')</a>
-                                    @endcan
-                                    @can('availability_delete')
-{!! Form::open(array(
-                                        'style' => 'display: inline-block;',
-                                        'method' => 'DELETE',
-                                        'onsubmit' => "return confirm('".trans("quickadmin.qa_are_you_sure")."');",
-                                        'route' => ['admin.availabilities.destroy', $availability->id])) !!}
-                                    {!! Form::submit(trans('quickadmin.qa_delete'), array('class' => 'btn btn-xs btn-danger')) !!}
-                                    {!! Form::close() !!}
-                                    @endcan
-                                </td>
-                                @endif
-                </tr>
-            @endforeach
-        @else
-            <tr>
-                <td colspan="10">@lang('quickadmin.qa_no_entries_in_table')</td>
-            </tr>
-        @endif
-    </tbody>
-</table>
-</div>
+    
 </div>
 
             <p>&nbsp;</p>
 
-            <a href="{{ route('frontend.rooms.index') }}" class="btn btn-default">@lang('quickadmin.qa_back_to_list')</a>
+            <a href="{{ route('frontend.rooms.index') }}" class="btn btn-outline-info">@lang('quickadmin.qa_back_to_list')</a>
         </div>
     </div>
-@stop
-
-@section('javascript')
-    @parent
-    <script src="//cdn.ckeditor.com/4.5.4/full/ckeditor.js"></script>
-    <script>
-        $('.editor').each(function () {
-                  CKEDITOR.replace($(this).attr('id'),{
-                    filebrowserImageBrowseUrl: '/laravel-filemanager?type=Images',
-                    filebrowserImageUploadUrl: '/laravel-filemanager/upload?type=Images&_token={{csrf_token()}}',
-                    filebrowserBrowseUrl: '/laravel-filemanager?type=Files',
-                    filebrowserUploadUrl: '/laravel-filemanager/upload?type=Files&_token={{csrf_token()}}'
-            });
-        });
-    </script>
-
 @stop
