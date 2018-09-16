2806ad8b56faaa40cf0217e44533fe9ecb1db893
kolydart <kolydart@macbeth.gateweb.gr>
2018-09-16 14:54:43 +0300

    set page seed for home

--- database/seeds/ContentPageSeed.php
+++ database/seeds/ContentPageSeed.php
@@ -16,7 +16,8 @@ class ContentPageSeed extends Seeder
     {
         $items = [
             
-            ['id' => 1, 'title' => 'About us', 'page_text' => 'Sample text', 'excerpt' => 'Sample excerpt', 'featured_image' => '',],
+            ['id' => 1, 'alias'=>'about', 'title' => 'About us', 'page_text' => 'Sample text', 'excerpt' => 'Sample excerpt', 'featured_image' => '',],
+            ['id' => 2, 'alias'=>'home', 'title' => 'Home', 'page_text' => '<div class="row mb-4"> <div class="col-sm-4 my-auto"> <img src="/img/uoa-logo.png" class="img-responsive img-fluid" alt="uoa logo"> </div> <div class="col-sm-8"> <img src="/img/banner.jpg" class="img-responsive img-fluid img-thumbnail" alt="conference logo"> </div> </div> <div class="bs-component mb-5"> <div class="jumbotron"> <h1 class="display-3">Oι Τέχνες στο ελληνικό σχολείο: παρόν και μέλλον</h1> <p class="lead">Συνέδριο 11, 12, 13 Οκτωβρίου 2018</p> <p class="lead">Φιλοσοφική Σχολή</p></p> <hr class="my-4"> <p>Κύριος σκοπός του συνεδρίου είναι η ανάδειξη της κρίσιμης θέσης των τεχνών στον σχεδιασμό της εκπαιδευτικής πολιτικής για το μελλοντικό σχολείο και η παραγωγή εκπαιδευτικού υλικού.</p> <p class="lead"> <a class="btn btn-primary btn-lg mt-3" href="#" role="button"><i class="fab fa-get-pocket"></i> Θέλω να το παρακολουθήσω</a> </p> </div> </div>', 'excerpt' => 'Sample excerpt', 'featured_image' => '',],
 
         ];
 
