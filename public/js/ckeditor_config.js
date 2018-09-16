3575f12ddc7e4f40c190a1c5b1b018d7563d2247
kolydart <kolydart@macbeth.gateweb.gr>
2018-09-16 19:59:47 +0300

    ckeditor: FontAwesome tags are removed when switching between Source & html

--- public/js/ckeditor_config.js
+++ public/js/ckeditor_config.js
@@ -1,4 +1,6 @@
 CKEDITOR.editorConfig = function( config ) {
 	config.htmlEncodeOutput = false;
 	config.entities = false;
+	config.protectedSource.push( /<i class[\s\S]*?\>/g );
+	config.protectedSource.push( /<\/i>/g );
 };
