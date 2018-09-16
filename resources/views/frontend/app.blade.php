a94d4b869e98983e66143412b45e12de5bc4bd11
kolydart <kolydart@macbeth.gateweb.gr>
2018-09-16 10:57:23 +0300

    datatables greek language (had to replace jquery to 1.11)

--- resources/views/frontend/app.blade.php
+++ resources/views/frontend/app.blade.php
@@ -16,6 +16,13 @@
           href="//cdn.datatables.net/buttons/1.2.4/css/buttons.dataTables.min.css"/>
 
     <title>@yield('title',config('app.name'))</title>
+    
+    <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
+    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+    <!--[if lt IE 9]>
+    <script src="https://oss.maxcdn.com/html5shiv/3.7.3/html5shiv.min.js"></script>
+    <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+    <![endif]-->
 
     {{-- <link rel="icon" href="/favicon.ico"> --}}
     {{-- <link href="{{ mix('/css/app.css') }}" rel="stylesheet" > --}}
@@ -49,10 +56,10 @@
     {{-- Bootstrap core JavaScript
     ================================================== 
     Placed at the end of the document so the pages load faster  --}}
-    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
-    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
-
+{{-- <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script> --}}
+<script src="//code.jquery.com/jquery-1.11.3.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
 
     <script src="//cdn.datatables.net/1.10.9/js/jquery.dataTables.min.js"></script>
     <script src="//cdn.datatables.net/buttons/1.2.4/js/dataTables.buttons.min.js"></script>
@@ -70,9 +77,25 @@
     <script src="{{ url('adminlte/plugins/fastclick/fastclick.js') }}"></script>
     {{-- <script src="{{ url('adminlte/js/app.min.js') }}"></script> --}}
 
+<script>
+    window.deleteButtonTrans = '{{ trans("quickadmin.qa_delete_selected") }}';
+    window.copyButtonTrans = '{{ trans("quickadmin.qa_copy") }}';
+    window.csvButtonTrans = '{{ trans("quickadmin.qa_csv") }}';
+    window.excelButtonTrans = '{{ trans("quickadmin.qa_excel") }}';
+    window.pdfButtonTrans = '{{ trans("quickadmin.qa_pdf") }}';
+    window.printButtonTrans = '{{ trans("quickadmin.qa_print") }}';
+    window.colvisButtonTrans = '{{ trans("quickadmin.qa_colvis") }}';
+</script>
+
+<script>
+    $.extend(true, $.fn.dataTable.defaults, {
+        "language": {
+            "url": "https://cdn.datatables.net/plug-ins/1.10.16/i18n/Greek.json"
+        }
+    });
+</script>
+
     @yield('javascript')
-
-
 
   </body>
 </html>
