628f229f00c79c5a077e3b7b59f3f051a0fc9d8c
kolydart <kolydart@macbeth.gateweb.gr>
2018-09-16 21:07:41 +0300

    ckeditor allow content

--- public/js/ckeditor_config.js
+++ public/js/ckeditor_config.js
@@ -1,6 +1,9 @@
 CKEDITOR.editorConfig = function( config ) {
+	// display characters (rawurldecode)
 	config.htmlEncodeOutput = false;
 	config.entities = false;
+	// allow custom code (fontawesome, classes)
 	config.protectedSource.push( /<i class[\s\S]*?\>/g );
 	config.protectedSource.push( /<\/i>/g );
+	config.allowedContent = true;
 };
