96199f6e6a1ee119dfc577287e493d4c0a901d88
kolydart <kolydart@macbeth.gateweb.gr>
2018-09-17 08:45:54 +0300

    improve seed & re-enable registration

--- database/factories/AvailabilityFactory.php
+++ database/factories/AvailabilityFactory.php
@@ -1,13 +1,17 @@
 <?php
 
 use App\Room;
+use gateweb\common\DateTime;
 
 $factory->define(App\Availability::class, function (Faker\Generator $faker) {
+	$date = $faker->dateTimeBetween($startDate = '2018-10-11', $endDate = '2018-10-14')->format('Y-m-d H:m:s');
+	$obj = (new DateTime($date))->setTime($faker->numberBetween(10,18),00);
+
     return [
-        "room_id" => Room::all()->random(),
+        "room_id" => collect(Room::all()->random()->id,null)->random(),
         "color_id" => factory('App\Color')->create(),
-        "start" => $faker->date("Y-m-d H:i:s", $max = 'now'),
-        "end" => $faker->date("Y-m-d H:i:s", $max = 'now'),
+        "start" => $obj->sql(),
+        "end" => $obj->modify('+'.collect([1,2,3,])->random().' hour')->sql(),
         "notes" => $faker->sentence,
     ];
 });
