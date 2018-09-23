a49483ed6d0fc3e12c0fb7eeee00fbb18dbe0f83
kolydart <kolydart@macbeth.gateweb.gr>
2018-09-23 12:35:26 +0300

    edit page button!

--- resources/views/frontend/page.blade.php
+++ resources/views/frontend/page.blade.php
@@ -4,6 +4,9 @@
 
 @section('content')
 
+	@can('content_page_edit')
+		<a href="{{route('admin.content_pages.edit',App\ContentPage::where('alias',$page->alias)->firstOrFail()->id)}}" class="btn btn-warning"><i class="fa fa-edit"></i> @lang('Edit')</a>
+	@endif
 	{!!$page->excerpt!!}
 	{!!$page->page_text!!}
 
