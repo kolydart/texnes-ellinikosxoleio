b07e9b151ce429ef9d6c15d52b0c09fa5904a5f1
kolydart <kolydart@macbeth.gateweb.gr>
2018-09-25 21:21:33 +0300

    frontpage.home register button change href if user is logged-in

--- resources/views/frontend/home.blade.php
+++ resources/views/frontend/home.blade.php
@@ -6,6 +6,6 @@
 		<a href="{{route('admin.content_pages.edit',3)}}" class="btn btn-warning btn-sm m-4"><i class="fa fa-edit"></i> @lang('Edit')</a>
 	@endif
 
-	{!! App\ContentPage::where('alias','home')->first()->page_text !!}
+	{!! str_replace('href="/register"', 'href="/page/faq"', App\ContentPage::where('alias','home')->first()->page_text); !!}
 	
 @endsection
