a40bcb465c3e1ae857418cff179a9346f4125428
kolydart <kolydart@macbeth.gateweb.gr>
2018-09-26 19:42:02 +0300

    fix forgotten check in 6.1.5

--- resources/views/frontend/home.blade.php
+++ resources/views/frontend/home.blade.php
@@ -6,6 +6,11 @@
 		<a href="{{route('admin.content_pages.edit',3)}}" class="btn btn-warning btn-sm m-4"><i class="fa fa-edit"></i> @lang('Edit')</a>
 	@endif
 
-	{!! str_replace('href="/register"', 'href="/page/faq"', App\ContentPage::where('alias','home')->first()->page_text); !!}
+	@if (Auth::check())
+		{!! str_replace('href="/register"', 'href="/page/faq"', App\ContentPage::where('alias','home')->first()->page_text); !!}
+	@else
+		{!! App\ContentPage::where('alias','home')->first()->page_text !!}
+	@endif
+
 	
 @endsection
