7c113a4f7386f30fdd6a3501a1d2b3b36565c06e
kolydart <kolydart@macbeth.gateweb.gr>
2018-12-27 14:47:33 +0200

    add Fullpaper::boot() method for uuid. remove uuid form $fillable

--- app/Fullpaper.php
+++ app/Fullpaper.php
@@ -24,9 +24,19 @@ class Fullpaper extends Model implements HasMedia
 
     use SoftDeletes, HasMediaTrait;
 
-    protected $fillable = ['description', 'uuid', 'paper_id'];
+    protected $fillable = ['description', 'paper_id'];
     protected $hidden = [];
     
+    /**
+     *  Setup model event hooks
+     */
+    public static function boot()
+    {
+        parent::boot();
+        self::creating(function ($model) {
+            $model->uuid = (string) \Uuid::generate(4);
+        });
+    }
     
 
     /**
