6a96574d2edb2fd94cec3eaa61363de011796a3c
kolydart <kolydart@macbeth.gateweb.gr>
2019-05-22 10:09:56 +0300

    disable frontend.lunch.confirm in order to enable cache

--- routes/gw.php
+++ routes/gw.php
@@ -25,17 +25,17 @@ Route::get('/attend/', 'Frontend\AttendsController@index')->name('frontend.atten
 Route::get('/attend/{paper_id}', 'Frontend\AttendsController@create')->name('frontend.attend.create');
 Route::get('/attend/delete/{paper_id}', 'Frontend\AttendsController@delete')->name('frontend.attend.delete');
 
-Route::get('/lunch/confirm/{lunch_id}', function (\Illuminate\Http\Request $request) {
-    if (! $request->hasValidSignature()) {
-	    \gateweb\common\Presenter::message("<h4>Η προθεσμία επιβεβαίωσης παρήλθε. Επικοινωνήστε με την γραμματεία.</h4>","warning");
-    }else{
-	    $lunch = \App\Lunch::findOrFail($request->lunch_id);
-	    $lunch->update(['confirm' => 'confirmed']);
-	    \gateweb\common\Presenter::message("<h4>Η δήλωσή σας για το γεύμα επιβεβαιώθηκε ($lunch->menu)</h4>","success");
-    }
-    return redirect(route('frontend.home'));
+// Route::get('/lunch/confirm/{lunch_id}', function (\Illuminate\Http\Request $request) {
+//     if (! $request->hasValidSignature()) {
+// 	    \gateweb\common\Presenter::message("<h4>Η προθεσμία επιβεβαίωσης παρήλθε. Επικοινωνήστε με την γραμματεία.</h4>","warning");
+//     }else{
+// 	    $lunch = \App\Lunch::findOrFail($request->lunch_id);
+// 	    $lunch->update(['confirm' => 'confirmed']);
+// 	    \gateweb\common\Presenter::message("<h4>Η δήλωσή σας για το γεύμα επιβεβαιώθηκε ($lunch->menu)</h4>","success");
+//     }
+//     return redirect(route('frontend.home'));
 
-})->name('frontend.lunch.confirm');
+// })->name('frontend.lunch.confirm');
 
 /* edit lab with signed url */
 Route::get('/papers/{paper}/edit', 'Frontend\PapersController@edit')->name('frontend.papers.edit');
@@ -47,7 +47,4 @@ Route::put('/papers/{paper}', 'Frontend\PapersController@update')->name('fronten
 Route::get('/fullpapers/{uuid}/download/{paper_id?}', 'Frontend\FullpapersController@download')->name('frontend.fullpapers.download');
 
 /** disable registration */
-Route::any('/register', function() {
-	\gateweb\common\Presenter::message(__('Registration is disabled'));
-    return redirect('/');
-});
+Route::redirect('/register', '/');
