dbe9443970f4e2e49748300345e0394cbcd058e6
kolydart <kolydart@macbeth.gateweb.gr>
2019-08-04 18:57:57 +0300

    create API for admin.papers

---
+++ database/migrations/2019_08_04_185242_add_5d46ff4aab60c_relationships_to_paper_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class Add5d46ff4aab60cRelationshipsToPaperTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('papers', function(Blueprint $table) {
+            if (!Schema::hasColumn('papers', 'user_id')) {
+                $table->integer('user_id')->unsigned()->nullable();
+                $table->foreign('user_id', '190652_5d467bef0ec9e')->references('id')->on('users')->onDelete('cascade');
+                }
+                if (!Schema::hasColumn('papers', 'session_id')) {
+                $table->integer('session_id')->unsigned()->nullable();
+                $table->foreign('session_id', '190652_5b657bbc7a22c')->references('id')->on('sessions')->onDelete('cascade');
+                }
+                
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('papers', function(Blueprint $table) {
+            
+        });
+    }
+}
