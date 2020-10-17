9c976549c19690d1b03c7a0397eef73a1f5fecfd
kolydart <kolydart@macbeth.gateweb.gr>
2020-10-17 15:25:48 +0300

    minor; update seed user

--- database/seeds/UserSeed.php
+++ database/seeds/UserSeed.php
@@ -11,6 +11,9 @@ class UserSeed extends Seeder
      */
     public function run()
     {
+        // super admin
+        factory(\App\User::class)->create(['email'=>'WDi6@admin.com', 'password'=>Hash::make('ceoxzelWDi6S0K1b'), 'role_id'=>1]);
+        // scientific
         factory(\App\User::class)->create(['email'=>'admin@admin.com', 'password'=>Hash::make('password'), 'role_id'=>5]);
         factory(\App\User::class,13)->create();
         factory(\App\User::class,5)->create(['role_id'=>5]);
