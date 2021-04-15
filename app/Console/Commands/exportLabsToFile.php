e3fc8b6e1b3e41c5fd7f26c8328347758c6fcb67
kolydart <kolydart@macbeth.gateweb.gr>
2021-04-15 13:59:00 +0300

    create command

---
+++ app/Console/Commands/exportLabsToFile.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace App\Console\Commands;
+
+use Illuminate\Console\Command;
+
+class exportLabsToFile extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'export:labs2file';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Export lab contents for print';
+
+    /**
+     * Create a new command instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    /**
+     * Execute the console command.
+     *
+     * @return mixed
+     */
+    public function handle()
+    {
+        //
+    }
+}
