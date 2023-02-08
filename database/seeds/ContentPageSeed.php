c35e967fff587184c84d90336fafcc6d22fc03b6
kolydart <kolydar@macbeth.opera.gateweb.gr>
2023-02-08 13:04:52 +0200

    update Content Page Seeder

--- database/seeds/ContentPageSeed.php
+++ database/seeds/ContentPageSeed.php
@@ -17,7 +17,27 @@ class ContentPageSeed extends Seeder
         $items = [
             
             ['id' => 1, 'alias'=>'about', 'title' => 'About us', 'page_text' => 'Sample text', 'excerpt' => 'Sample excerpt', 'featured_image' => '',],
-            ['id' => 2, 'alias'=>'home', 'title' => 'Home', 'page_text' => '<div class="row mb-4"> <div class="col-sm-4 my-auto"> <img src="/img/uoa-logo.png" class="img-responsive img-fluid" alt="uoa logo"> </div> <div class="col-sm-8"> <img src="/img/banner.jpg" class="img-responsive img-fluid img-thumbnail" alt="conference logo"> </div> </div> <div class="bs-component mb-5"> <div class="jumbotron"> <h1 class="display-3">Oι Τέχνες στο ελληνικό σχολείο: παρόν και μέλλον</h1> <p class="lead">Συνέδριο 11, 12, 13 Οκτωβρίου 2018</p> <p class="lead">Φιλοσοφική Σχολή</p></p> <hr class="my-4"> <p>Κύριος σκοπός του συνεδρίου είναι η ανάδειξη της κρίσιμης θέσης των τεχνών στον σχεδιασμό της εκπαιδευτικής πολιτικής για το μελλοντικό σχολείο και η παραγωγή εκπαιδευτικού υλικού.</p></div> </div>', 'excerpt' => 'Sample excerpt', 'featured_image' => '',],
+            ['id' => 2, 'alias'=>'home', 'title' => 'Home', 'page_text' => '
+<div class="row mb-4">
+    <div class="col-sm-4 my-auto"> <img src="/img/uoa-logo.png" class="img-responsive img-fluid" alt="uoa logo"> </div>
+    <div class="col-sm-8"> <img src="/img/banner.jpg" class="img-responsive img-fluid img-thumbnail" alt="conference logo"> </div>
+</div>
+<div class="bs-component mb-5">
+    <div class="jumbotron">
+        <h1 class="display-3">Επίδειξη εφαρμογής Συστήματος Διαχείρισης Συνεδρίων</h1>
+        <p class="lead">Demo application of the Conference Management System</p>
+        <p class="lead">Conference venue</p>
+        </p>
+        <hr class="my-4">
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+        tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+        quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+        consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+        cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+        proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+    </div>
+</div>
+            ', 'excerpt' => 'Sample excerpt', 'featured_image' => '',],
 
         ];
 
